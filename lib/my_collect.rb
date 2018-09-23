a151aff94404bff3836e6509a01c2618ef8e0b17
David masterson <david73180@gmail.com>
2018-09-23 20:35:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,21 +1,4 @@
-def my_collect(array)
-  i = 0 
- while i < array.length 
-   yield (array[i])
-   i = i + 1 
-  end
-  return $names
-end
 
-
-names = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
-$names = []
-
-z = 0 
-
-  if z < 1
-    my_collect(names) do |x|
-    $names =$names.push(x.split(" ").first)
     
 
 
